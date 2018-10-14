2feb777bfb968675e27b74635f90df3948920267
fourbit <fourbit@yandex.ru>
2018-10-14 17:31:25 +0500

    init

---
+++ database/migrations/auth_url_create_table_auth_url_users.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAuthUrlUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auth_url_users', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on(config('auth_utl.migration.users'));
+
+            $table->string('hash');
+            $table->string('params');
+
+            $table->timestamps();
+
+            $table->index('hash');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auth_url_users');
+    }
+}
---
+++ src/Models/AuthUrl.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuthUrl extends Model
+{
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->setTable(config('auth_url.migration.auth_urls'));
+    }
+}
